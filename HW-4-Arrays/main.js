3a7a8be18629b58336751709f0f19f45cb579a2d
Shulhan Dmytro <shulhan.d.v@gmail.com>
2022-06-02 12:37:43 +0300

    Update code

--- HW-4-Arrays/main.js
+++ HW-4-Arrays/main.js
@@ -1,14 +1,14 @@
 "use strict"
 
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
-const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
-const marks = [4, 5, 5, 3, 4, 5];
+const STUDENTS = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const THEMES = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const MARKS = [4, 5, 5, 3, 4, 5];
 
 // 1. Розділіть студентів на пари(хлопець + дівчина) для работи над проєктом.
 
-const arrPairs = [];
 function getPairs() {
-    let copyArr = [...students];
+    const arrPairs = [];
+    let copyArr = [...STUDENTS];
     copyArr.splice (1, 2, "Олена", "Ігор");
     for (let i = 0; i < copyArr.length; i += 2) {
         arrPairs.push([copyArr[i], copyArr[i + 1]]);
@@ -18,29 +18,31 @@ function getPairs() {
 
 // // 2. Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати.
 
-const arrProjects = [];
 function getProjectForPair() {
+    const arrProjects = [];
     for (let i = 0; i < pairs.length; i++) {
-        arrProjects.push([pairs[i][0] + ' та ' + pairs[i][1], themes[i]]);
+        arrProjects.push([pairs[i][0] + ' та ' + pairs[i][1], THEMES[i]]);
     }
     return arrProjects;
 };
 
 // // 3. Зіставте оцінки(marks) зі студентом(students).
 
-const arrMarks = [];
 function markForStudent() {
-    for (let i = 0; i < students.length; i++) {
-        arrMarks.push([students[i], marks[i]]);
+    const arrMarks = [];
+    for (let i = 0; i < STUDENTS.length; i++) {
+        arrMarks.push([STUDENTS[i], MARKS[i]]);
     }
     return arrMarks;
 };
 
 // // 4. Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт.
-const arrRandomMarks = [];
+
 function randomMarkForPair() {
-    for (let i = 0; i < arrProjects.length; i++) {
-        arrRandomMarks.push([pairs[i][0] + ' та ' + pairs[i][1], themes[i], Math.round(Math.random() * 4 + 1)]);
+    const arrRandomMarks = [];
+    for (let i = 0; i < projects.length; i++) {
+        // arrRandomMarks.push([pairs[i][0] + ' та ' + pairs[i][1], themes[i], Math.round(Math.random() * 4 + 1)]);
+        arrRandomMarks.push([projects[i].join(", "), Math.round(Math.random() * 4 + 1)]);
     }
     return arrRandomMarks;
 };
